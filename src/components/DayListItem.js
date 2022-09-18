6c295fb572a4d042d3904d36a60b3e3fdbd1ef0f
mjuned <mmj.nowaz@gmail.com>
2022-09-18 02:20:39 +0000

    Add an run test for DayListItem but 2 failing

---
+++ src/components/DayListItem.js
@@ -0,0 +1,32 @@
+import React from "react";
+import classNames from "classnames";
+
+import "components/DayListItem.scss";
+
+const spotsMessage = spots => {
+  if (!spots) {
+    return 'no spots remaining';
+  };
+
+  if (spots === 1) {
+    return `${spots} spot remaining`;
+  } else {
+    return `${spots} spots remaining`;
+  };
+};
+
+export default function DayListItem(props) {
+  const message = spotsMessage(props.spots);
+
+  const dayClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': props.spots === 0
+ });
+
+  return (
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{props.spots} spots remaining</h3>
+    </li>
+  );
+};
