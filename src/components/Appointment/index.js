337649131f9dcc5bfa5004d7a242031b38fe2867
mjuned <mmj.nowaz@gmail.com>
2022-09-21 19:08:46 +0000

    Complete displaying appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -8,6 +8,9 @@ import Show from "components/Appointment/Show.js";
 
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show student={props.student} interviewer={props.interviewer} /> : <Empty/>}
+    </article>
   );
 };
